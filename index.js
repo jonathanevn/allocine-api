7dc6e1b405e73c4276fc19d0bfdc46ef339008db
Jonathan EVENO <jonathaneveno@mbp-de-jonathan.lan>
2018-11-07 16:54:27 +0100

    update index.js

--- index.js
+++ index.js
@@ -84,7 +84,8 @@ app.post("/api/sign_up", function(req, res) {
         _id: newUser._id,
         token: newUser.token,
         email: newUser.email,
-        lists: newUser.lists
+        lists: newUser.lists,
+        toto: "test"
       });
     }
   });
