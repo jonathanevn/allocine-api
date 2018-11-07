8e4e3eed7b366897ed9d45a16774e1bb44e34fc1
Jonathan EVENO <jonathaneveno@mbp-de-jonathan.lan>
2018-11-07 11:54:14 +0100

    test

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+const axios = require("axios");
+
+const mongoose = require("mongoose");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/allocine-api"
+);
+
+app.listen(process.env.PORT || 3000);
